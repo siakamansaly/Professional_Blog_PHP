e4fc5a4a09ebb0d0ce205dfaffff87fb96b36f93
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-02-16 11:58:39 +0100

    Fix bug modal.js

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
     function GetURLParameter(sParam) {
         var sPageURL = window.location.search.substring(1);
-        var sURLVariables = sPageURL.split('&');
+        var sURLVariables = sPageURL.split("&");
         for (var i = 0; i < sURLVariables.length; i++) {
-            var sParameterName = sURLVariables[i].split('=');
-            if (sParameterName[0] == sParam) {
+            var sParameterName = sURLVariables[i].split("=");
+            if (sParameterName[0] === sParam) {
                 return sParameterName[1];
             }
         }
     }
-    var url_register = GetURLParameter('register');
-    var url_login = GetURLParameter('login');
-    if (url_register == 'true') {
-        $('#register').modal('show')
+    var urlRegister = GetURLParameter("register");
+    var urlLogin = GetURLParameter("login");
+    if (urlRegister === "true") {
+        $("#register").modal("show");
     }
-    if (url_login == 'true') {
-        $('#login').modal('show')
+    if (urlLogin === "true") {
+        $("#login").modal("show");
     }
 });
