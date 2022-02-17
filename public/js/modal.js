b9e415f4612a088abcdf21dbc29ab781b72b1710
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-02-17 10:12:08 +0100

    Fix bug : Generic Object Injection Sink

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,16 +1,10 @@
 $(document).ready(function () {
-    function GetURLParameter(sParam) {
-        var sPageURL = window.location.search.substring(1);
-        var sURLVariables = sPageURL.split("&");
-        for (var i = 0; i < sURLVariables.length; i++) {
-            var sParameterName = sURLVariables[i].split("=");
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1];
-            }
-        }
+       $.urlParam = function(name){
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        return results[1] || 0;
     }
-    var urlRegister = GetURLParameter("register");
-    var urlLogin = GetURLParameter("login");
+    var urlRegister = $.urlParam('register');
+    var urlLogin = $.urlParam('login');
     if (urlRegister === "true") {
         $("#register").modal("show");
     }
